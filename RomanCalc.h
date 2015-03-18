fe1f663f902bce4ffa3586ab9db16d7757484f84
rosiana <rosilutfi@gmail.com>
2015-03-18 21:25:11 +0700

    xxx

--- RomanCalc.h
+++ RomanCalc.h
@@ -4,6 +4,9 @@
 #include <iostream>
 #include <stdlib.h>
 #include <string>
+#include <cmath>
+#include <math.h>
+
 using namespace std;
 
 class RomanCalc {
@@ -13,8 +16,11 @@ class RomanCalc {
         int RomantoDec(const string& s);
         string DectoRoman(int d);
         string Calculate();
-		void SmallCalculate(Stack<float> &bil,Stack<string> &operatorx);
-		
+		void SmallCalculate(Stack<float> &bil, Stack<string> &operatorx);
+		int CalculateInfix();
+		int CalculatePrefix();
+		int CalculatePostfix();
+
     private:
         Stack<int> bil;
         Stack<string> operatorx;
