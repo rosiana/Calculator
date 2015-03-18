268efdd914e3045dcba25be3a27701f03e2b5020
kevin9huang <13512096@std.students.itb.ac.id>
2015-03-18 09:56:46 +0700

    tes5.cpp on progress untuk calculate() arab

--- tes5.cpp
+++ tes5.cpp
@@ -2,18 +2,19 @@
 using namespace std;
 #include <string>
 #include <stdlib.h>
-
+#include "Stack.h"
 
 int main(){
-	string tes = "3.44+6*6-(6/3)%8";
-	string operatorx[10];
-	float bil[10];
+	string tes = "3.44+6*6-(6/3)";
+	string delimiter[10] = {"+","-","/","%",":"};
+	Stack<string> operatorx;
+	Stack<float> bil;
 	int cbil = 0;
+	int m = 0;
 	int coperator = 0;
 	cout<<"Mulai"<<endl;
 	while(tes.length()!=0)
 	{
-		//temp = "";
 		char *temp =  new char[30];
 	
 		temp[0] = '@';
@@ -21,31 +22,30 @@ int main(){
 		while(tes[0] == '1' || tes[0] == '2' || tes[0] == '3' || tes[0] == '4' || tes[0] == '5'
 		|| tes[0] == '6'|| tes[0] == '7' || tes[0] == '8' || tes[0] == '9' || tes[0] == '.')
 		{	
-			//temp.append(tes.substr(0,1));
 			temp[ctemp] = tes[0];
 			tes.erase(0,1);
 			ctemp++;
 		}
 		if(temp[0] !='@')
 		{	
-			bil[cbil] = atof(temp);
+			bil<< atof(temp);
 			cbil++;
 		}
-		if(tes[0] == '+' || tes[0] == '-' || tes[0] == '*' || tes[0] == '/' || tes[0] == '%' || tes[0] == '('|| tes[0] == ')')
+		if(tes[0] == '+' || tes[0] == '-' || tes[0] == '*' || tes[0] == '/' || tes[0] == '('|| tes[0] == ')')
 		{
-			operatorx[coperator] = tes.substr(0,1);
+			operatorx<<tes.substr(0,1);
 			tes.erase(0,1);
 			coperator++;
 		}
 	}
 	cout<<"Stack bilangan"<<endl;
-	for(int i = 0;i<=cbil-1;i++)
+	for(int i = 0;i<=bil.getDataCount()-1;i++)
 	{
-		cout<<i<<"). "<<bil[i]<<endl;
+		cout<<i<<"). "<<bil.getData(i)<<endl;
 	}
 	cout<<"Stack operator"<<endl;
-	for(int j = 0;j<=coperator-1;j++)
+	for(int j = 0;j<=operatorx.getDataCount()-1;j++)
 	{
-		cout<<j<<"). "<<operatorx[j]<<endl;
+		cout<<j<<"). "<<operatorx.getData(j)<<endl;
 	}
 }
