060a143a2f835df4867203525356b631477a0fd0
kevin9huang <13512096@std.students.itb.ac.id>
2015-03-18 10:50:25 +0700

    tes5.cpp untuk calculate belum kelar

--- tes5.cpp
+++ tes5.cpp
@@ -3,7 +3,7 @@ using namespace std;
 #include <string>
 #include <stdlib.h>
 #include "Stack.h"
-
+#include <cmath>
 void Print(Stack<float> bil,Stack<string>operatorx){
 	cout<<"Stack bilangan"<<endl;
 	for(int i = 0;i<=bil.getDataCount()-1;i++)
@@ -17,10 +17,46 @@ void Print(Stack<float> bil,Stack<string>operatorx){
 	}
 }
 
+void SmallCalculate(Stack<float> &bil,Stack<string> &operatorx)
+{
+	float popbill;
+	float popbill2;
+	string dump;
+	popbil1 = bil.getLastData();
+	bil>>dump;
+	popbil2 = bil.getLastData();
+	bil>>dump;
+	popoperator = operatorx.getLastData();
+	operatorx>>dump;
+	cout<<"Popbil1 : "<<popbil1<<endl<<"Popbil2 : "<<popbil2<<endl<<"Operator"<<popoperator<<endl;
+	if(popoperator == "x")
+	{
+		bil<< popbil1*popbil2;
+	}
+	else if(popoperator == "/")
+	{
+		bil<< popbil1/popbil2;
+	}
+	else if(operatorx == "%")
+	{
+		bil<< popbil1%popbil2;
+	}
+	else if(operatorx == "+")
+	{
+		bil<< popbil1+popbil2;
+	}
+	else if(operatorx == "-")
+	{
+		bil<< popbil1-popbil2;
+	}
+}
+
 int main(){
 	string tes = "3.44+6*6-(6/3)";
 	Stack<string> operatorx;
+	string popbil1,popbil2,popoperator;
 	Stack<float> bil;
+	string dump;
 	int cbil = 0;
 	int m = 0;
 	int coperator = 0;
@@ -43,11 +79,36 @@ int main(){
 			bil<< atof(temp);
 			cbil++;
 		}
-		if(tes[0] == '+' || tes[0] == '-' || tes[0] == '*' || tes[0] == '/' || tes[0] == '('|| tes[0] == ')')
+		if(tes[0] == '+' || tes[0] == '-' || tes[0] == '*' || tes[0] == '/' || tes[0] == '%' || tes[0] == '('|| tes[0] == ')')
 		{
-			operatorx<<tes.substr(0,1);
-			tes.erase(0,1);
-			coperator++;
+			if(operatorx.isEmpty() || tes[0] == '(' || tes[0] == 'x' || tes[0] == '/' || tes[0] == '%')
+			{
+				operatorx<<tes.substr(0,1);
+				tes.erase(0,1);
+				coperator++;
+			}
+			else
+			{
+				if(tes[0] == '+' || tes[0] == '-' || tes[0] == ')')
+				{
+					operatorx<<tes.substr(0,1);
+					tes.erase(0,1);
+					coperator++;
+				}
+				if(tes[0] == '+' || tes[0] == '-')
+				{
+					if(operatorx.getLastData() == "x" || operatorx.getLastData() == "/" || operatorx.getLastData() == "%")
+					{
+						
+					}
+					else
+					{
+						operatorx<<tes.substr(0,1);
+						tes.erase(0,1);
+						coperator++;
+					}
+				}
+			}
 		}
 	}
 	return 0;
