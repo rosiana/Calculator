2a8b42b841fe726e687523482bb9191399fa7636
Aldyaka Mushofan <mushofan@users.noreply.github.com>
2015-03-19 08:19:32 +0700

    Udah bisa banyak digit

--- prefix.cpp
+++ prefix.cpp
@@ -38,7 +38,7 @@ int performOperation(char op, int op1, int op2)
 
 int main()
 {
-    char exp[1000], buffer[15];
+    char exp[1000], buffer[15], buffer2[15];
     int i,op1, op2, len, j, x;
     stack<int> s;
     printf("Enter a Prefix Expression:\n");
@@ -48,12 +48,22 @@ int main()
     for(i=len-1; i>=0; i--){
 
         if(exp[i]>='0' && exp[i]<='9'){
-            buffer[j++] = exp[i];
+            buffer[j] = exp[i];
+            //cout << buffer[j] << endl;
+            j++;
         }
         else if(exp[i]==' '){
             if(j>0){
-                buffer[j] = '\0';
-                x = atoi(buffer);
+				cout << "Nilai j: " << j << endl;
+				int n =j-1;
+				int k;
+                for(k = 0; k < j; k++){
+					buffer2[k]=buffer[n];
+					cout << buffer2[k]<<endl;
+					n--;
+                }
+                buffer2[k+1] = '\0';
+                x = atoi(buffer2);
                 s.push(x);
                 j = 0;
             }
